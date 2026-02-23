127ead7732e39e1032801d7c3382850f15597386
afneal <Afneal0226@gmail.com>
2026-02-23 16:29:20 -0600

    Added  mapping to activity controller, added activity model

--- travel-mate-backend/src/main/java/com/unit2project/travel_mate_backend/models/Activity.java
+++ travel-mate-backend/src/main/java/com/unit2project/travel_mate_backend/models/Activity.java
@@ -1,4 +1,48 @@
 package com.unit2project.travel_mate_backend.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+@Entity
 public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private int time;
+    @Lob                                    //large object, for longer text
+    private String notes;
+
+    @ManyToOne //many activities can belong to one day, does not need mappedBy since this is the owning side of the relationship and will have the foreign key
+    @JsonBackReference  //prevents infinite recursion during JSON serialization
+    private Day day;    //stores the day(parent) this activity belongs to
+
+    public Activity() {
+    }
+
+    public Activity(int id, String name, int time, String notes) {
+        this.id = id;
+        this.name = name;
+        this.time = time;
+        this.notes = notes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getActivityTime() {
+        return time;
+    }
+
+    public String getActivityNotes() {
+        return notes;
+    }
+
 }
