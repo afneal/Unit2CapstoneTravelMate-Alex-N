a4d60dbaa510d4d6b747bd92816b63e024a65fea
afneal <Afneal0226@gmail.com>
2026-02-24 14:40:51 -0600

    Added trip controller requests

--- travel-mate-backend/src/main/java/com/unit2project/travel_mate_backend/repositories/TripRepository.java
+++ travel-mate-backend/src/main/java/com/unit2project/travel_mate_backend/repositories/TripRepository.java
@@ -1,4 +1,8 @@
 package com.unit2project.travel_mate_backend.repositories;
 
-public interface TripRepository {
+import com.unit2project.travel_mate_backend.models.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TripRepository extends JpaRepository <Trip, Integer> {
+
 }
