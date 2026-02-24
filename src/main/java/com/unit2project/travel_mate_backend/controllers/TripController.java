2c0a126b3480fbf49e1284d05827c4dfc0fedf91
afneal <Afneal0226@gmail.com>
2026-02-24 15:53:00 -0600

    Added requests for trip controller

--- src/main/java/com/unit2project/travel_mate_backend/controllers/TripController.java
+++ src/main/java/com/unit2project/travel_mate_backend/controllers/TripController.java
@@ -2,10 +2,13 @@ package com.unit2project.travel_mate_backend.controllers;
 
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.unit2project.travel_mate_backend.dto.DayDTO;
+import com.unit2project.travel_mate_backend.dto.TripDTO;
 import com.unit2project.travel_mate_backend.models.Day;
 import com.unit2project.travel_mate_backend.models.Trip;
 import com.unit2project.travel_mate_backend.models.User;
 import com.unit2project.travel_mate_backend.repositories.TripRepository;
+import com.unit2project.travel_mate_backend.repositories.UserRepository;
 import jakarta.persistence.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
@@ -14,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 //@Id
@@ -43,14 +47,35 @@ public class TripController {
     @Autowired
     TripRepository tripRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("")
     public List<Trip> getAllTrips() {
         return tripRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getTripById(@PathVariable int id) throws NoResourceFoundException {
+        Trip trip = tripRepository.findById(id).orElse(null);
+        if (trip == null) {
+            throw new NoResourceFoundException(HttpMethod.GET, "/" + id, "Trip with id " + id + " not found");
+        } else {
+            return new ResponseEntity<>(trip, HttpStatus.OK);
+        }
+    }
+
+
     @PostMapping("/addTrip")
     public ResponseEntity<?> addNewTrip(@RequestBody TripDTO tripData) {
-        Trip trip = new Trip(tripData.getName(), tripData.getUser(), tripData.getDays());
+        User user = userRepository.findById(tripData.getUserId()).orElse(null);
+        List<Day> days = new ArrayList<>();
+        for (DayDTO day : tripData.getDays()) {
+            Day newDay = new Day(day.getCity(), day.getDate(), day.getActivities());
+            days.add(newDay);
+        }
+
+        Trip trip = new Trip(tripData.getName(), user, days);
         tripRepository.save(trip);
         return new ResponseEntity<>(trip, HttpStatus.CREATED);
     }
--- src/main/java/com/unit2project/travel_mate_backend/dto/DayDTO.java
+++ src/main/java/com/unit2project/travel_mate_backend/dto/DayDTO.java
@@ -1,4 +1,52 @@
 package com.unit2project.travel_mate_backend.dto;
 
+import com.unit2project.travel_mate_backend.models.Activity;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class DayDTO {
+
+    private int id;
+    private String city;
+    private LocalDate date;
+    private List<Activity> activities;
+
+    public DayDTO() {
+    }
+
+    public DayDTO(String city, LocalDate date, List<Activity> activities) {
+        this.city = city;
+        this.date = date;
+        this.activities = activities;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public List<Activity> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(List<Activity> activities) {
+        this.activities = activities;
+    }
 }
